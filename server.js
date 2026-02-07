81413c72d71c3f21fa01ec08cb855236d0ab8229
Octane <zorakotoasimbola@hotmail.com>
2026-02-07 21:53:00 +0300

    fix

--- server.js
+++ server.js
@@ -7,20 +7,16 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:4200', // URL de votre frontend Angular
-    credentials: true, // Permet l'envoi de cookies
+  origin: process.env.FRONTEND_URL || 'http://localhost:4200',
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/mall')
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mall')
   .then(() => console.log('MongoDB connecté'))
   .catch(err => console.error(err));
-
-// mongoose.connect('mongodb+srv://projetmean_db_user:ghBVzF5ZfPjdNTDf@cluster0.mqn9zli.mongodb.net/?appName=Cluster0')
-//   .then(() => console.log('MongoDB connecté'))
-//   .catch(err => console.error(err));
 
 
 app.use('/role', require('./routes/RoleRoute'));
